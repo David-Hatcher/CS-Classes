e7f82bb81dafad302fbac15b570d1bd55ca1de05
David <sallazar024@gmail.com>
2019-05-20 15:00:44 -0400

    Day 3

--- COP 2510/Day 2/Assignments/Ass2.java
+++ COP 2510/Day 2/Assignments/Ass2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Assignment2.src.main.java.summer.COP2510;
+package summer.COP2510;
 import java.util.Scanner;
 /**
  *
