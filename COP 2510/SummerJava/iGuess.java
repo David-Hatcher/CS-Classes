7ea2e867f084b9b1c8398b17be6be9ae52cc2888
David <Sallazar024@gmail.com>
2019-06-23 23:05:11 -0400

    Project almost finished

--- COP 2510/SummerJava/iGuess.java
+++ COP 2510/SummerJava/iGuess.java
@@ -1,22 +1,75 @@
 package COP2510.project.business;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
 
 public class iGuess {
-  static ArrayList<ArrayList<String>> listOfCombinations = new ArrayList<ArrayList<String>>();
-  static Random rand = new Random();
+  private static ArrayList<ArrayList<String>> combos = new ArrayList<ArrayList<String>>();
+  private static ArrayList<String> secretCode = new ArrayList<String>(); 
+  private static String[] possibles = {"v", "w", "x", "y", "z", "5", "6", "7", "8", "9", "=", "?", "%", "$"};
+  private static Random rand = new Random();
 
-  public static int RandGen(int minimum, int max) {
-    return rand.nextInt(max - minimum + 1) + minimum;
+  public static String getSecretCode(){
+    String code = "";
+    for (String character : secretCode) {
+      code += character;
+    }
+    return code;
+  }
+
+  public static void createLists() {
+    combos.clear();
+    for (int i = 0; i < possibles.length; i++) {
+        ArrayList<String> setStrings = new ArrayList<String>(Arrays.asList(possibles));
+        ArrayList<String> combo = new ArrayList<String>();
+        combo.add(setStrings.remove(i));
+        createListsRec(setStrings, combo, 5);
+    }
+  }
+
+  private static void createListsRec(ArrayList<String> set, ArrayList<String> combo, int length) {
+    ArrayList<String> currentSet = new ArrayList<String>(set);
+    for (int i = 0; i < currentSet.size(); i++) {
+        ArrayList<String> currentCombo = new ArrayList<String>(combo);
+        if (combo.size() == length - 1) {
+            // System.out.println(combo);
+            addAtsTolist(combo);
+            break;
+        } else if (!currentCombo.contains(currentSet.get(i))) {
+            currentCombo.add(currentSet.get(i));
+            createListsRec(currentSet, currentCombo, length);
+        }
+    }
+  }
+
+  private static void addAtsTolist(ArrayList<String> combo) {
+    for (int i = 0; i < combo.size() + 1; i++) {
+        ArrayList<String> comboCurrent = new ArrayList<String>(combo);
+        comboCurrent.add(i, "@");           
+        // System.out.println(comboCurrent);
+        combos.add(comboCurrent);            
+    }
+  }
+
+  public static ArrayList<String> generateSecretCode(){
+    secretCode.clear();
+    ArrayList<String> choices = new ArrayList<String>(Arrays.asList(possibles));
+    int spaceForAt = rand.nextInt(5);
+    for(int i = 0; i < 5; i++){
+      if(spaceForAt == i){
+        secretCode.add("@");
+      }
+      else{
+        secretCode.add(choices.remove(rand.nextInt(choices.size())));
+      }
+    }        
+    return secretCode;
+  }
+
+  public static ArrayList<String> generateRandomGuess(){    
+    return combos.get(rand.nextInt(combos.size()));
   }
 
   public static String compareScores(ArrayList<String> guess, ArrayList<String> combination){
@@ -36,83 +89,12 @@ public class iGuess {
   }
   
   public static void pruneList(ArrayList<String> guess, String score){
-    for (ArrayList<String> combination : listOfCombinations) {
-      if(!score.equalsIgnoreCase(compareScores(guess,combination))){
-        listOfCombinations.remove(combination);
+    ArrayList<ArrayList<String>> tempList = new ArrayList<ArrayList<String>>();
+    for (ArrayList<String> combination : combos) {
+      if(score.equalsIgnoreCase(compareScores(guess,combination))){
+        tempList.add(combination);
       }
     }
+  combos = tempList;
   }
-
-  public static String[] ArrayBuilder() {
-    ArrayList<String> possibles = new ArrayList<String>(
-        Arrays.asList("v", "w", "x", "y", "z", "5", "6", "7", "8", "9", "=", "?", "%", "$"));
-    String[] combination = new String[5];
-    int randForAt = RandGen(0, 4);
-    for (int i = 0; i < 5; i++) {
-      if (i == randForAt) {
-        combination[i] = "@";
-      } else {
-        String tempChar = possibles.get(RandGen(0, possibles.size() - 1));
-        combination[i] = tempChar;
-        possibles.remove(tempChar);
-      }
-    }
-    return combination;
-  }
-
-  public static ArrayList<ArrayList<String>> FullPossibles() {
-    ArrayList<ArrayList<String>> allCombos = new ArrayList<ArrayList<String>>();
-    while (allCombos.size() < 120120) {
-      String[] currentPossible = ArrayBuilder();
-      while (allCombos.contains(Arrays.asList(currentPossible))) {
-        currentPossible = ArrayBuilder();
-      }
-      ArrayList<String> currentToAdd = new ArrayList<String>();
-      for (String str : currentPossible) {
-        currentToAdd.add(str);
-      }
-      allCombos.add(currentToAdd);
-    }
-    return allCombos;
-  }
-
-  public static void writeFile(ArrayList<ArrayList<String>> allCombos ,String filePath) throws IOException {
-    PrintWriter out = new PrintWriter(
-                      new BufferedWriter(
-                      new FileWriter(filePath,true)));
-      for (ArrayList<String> combo : allCombos) {
-        int i = 0;
-        for (String character : combo) {        
-        if(i == 4){
-          out.println(character);
-        }
-        else{
-          out.print(character + ",");
-        }
-        i++;
-      }      
-    }
-    out.close();
-  }
-  public static void readFile(String filePath) throws IOException {
-    BufferedReader in = new BufferedReader(
-                        new FileReader(filePath));
-    ArrayList<ArrayList<String>> readCombos = new ArrayList<ArrayList<String>>(10);
-    String line;
-    while((line = in.readLine()) != null){      
-      String[] lineArray = line.split(",");
-      ArrayList<String> currentCombo = new ArrayList<String>();
-      for (String str : lineArray) {
-        currentCombo.add(str);
-      }
-      readCombos.add(currentCombo);      
-    }
-    in.close();
-    listOfCombinations = readCombos;    
-  }
-
-  public static void main(String[] args){
-    
-  }
-  
 }
--- COP 2510/SummerJava/projApp.java
+++ COP 2510/SummerJava/projApp.java
@@ -1,10 +1,205 @@
 package COP2510.project.UI;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import COP2510.project.business.iGuess;
 
 public class projApp{
+    private static Scanner read = new Scanner(System.in);
+    private static ArrayList<String> choicesSet = new ArrayList<String>(
+        Arrays.asList("v", "w", "x", "y", "z", "5", "6", "7", "8", "9", "=", "?", "%", "@", "$"));
+    
+    private static void print(String message){
+        System.out.print(message);
+    }
+    private static void println(String message){
+        System.out.println(message);
+    }
+    
+    private static String readString(String message){
+        print(message);
+        return read.nextLine();
+    }
+    private static void printBar(){
+        println("*************************************************");
+    }
+
+    private static String convertArrayListToString(ArrayList<String> code){
+        String codeString = "";
+        for (String character : code) {
+          codeString += character.toLowerCase();
+        }
+        return codeString;
+    }
+
+    private static ArrayList<String> convertStringToArrayList(String code){
+        ArrayList<String> codeArrayList = new ArrayList<String>();
+        for(int i = 0; i < code.length(); i++){
+            codeArrayList.add(Character.toString(code.charAt(i)).toLowerCase());
+        }
+        return codeArrayList;
+    }
+
+    private static void RunProgram(){
+        String message = ("Welcome to bull and cows:\n" +
+                          "To play please select from the following options.\n" +
+                          "1. To have the computer create a secret code and you guess it.(Enter 1)\n" +
+                          "2. To select a secret code yourself and the computer guesses it.(Enter 2)\n" +
+                          "3. To have the computer select a secret code then guess another computers secret code.(Enter 3)");
+        String choice = readString(message);
+        selectOption(choice);
+    }
+    private static void selectOption(String choice){
+        if(choice.equalsIgnoreCase("1")){
+            runComputerCode();
+        }
+        else if(choice.equalsIgnoreCase("2")){
+            runPlayerCode();
+        }
+        else if (choice.equalsIgnoreCase("3")){
+            runComputerCodeAndGuess();
+        }
+        else{
+            println("Incorrect Input!!!!");
+            RunProgram();
+        }
+    }
+
+    private static void runComputerCode(){
+        println("The Computer will randomly generate a code and you must guess it!");
+        println("Please type for guess of 5 digits, one digit must be @, the other 4 must be any"+
+                "\ncombination of the following, without repetion:\n"+
+                choicesSet+ 
+                "\n or type ***** to give up and have the secret code displayed.\n");        
+        String playerGuessString = askForGuess();
+        ArrayList<String> playerGuessArrayList = convertStringToArrayList(playerGuessString);
+        ArrayList<String> secretCodeComputerArrayList = iGuess.generateSecretCode();
+        // String secretCodeComputerString = convertArrayListToString(secretCodeComputerArrayList);
+        String score = "";
+        int guessCount = 0;
+        while(!score.equalsIgnoreCase("5A0B")){
+            if(doesPlayerGiveUp(playerGuessString)){
+                print("Good try!\nThe Secret Code is: " + iGuess.getSecretCode());
+                break;
+            }
+            else if(!ensureGuessCorrect(playerGuessString, playerGuessArrayList)){
+                println("ERROR, please ensure correct formatting per instructions.");
+            }
+            else{
+                score = iGuess.compareScores(playerGuessArrayList, secretCodeComputerArrayList);
+                println("Guess = " + playerGuessString + "\nScore = " + score + "\nGuess Count = " + ++guessCount);
+            }
+            if(!score.equalsIgnoreCase("5A0B")){
+                playerGuessString = askForGuess();
+                playerGuessArrayList = convertStringToArrayList(playerGuessString);
+            }
+        }
+        didPlayerGuessCorrectly(score);
+    }
+
+
+    private static void didPlayerGuessCorrectly(String score){
+        if(score.equalsIgnoreCase("5A0B")){
+            println("Great job, you win!");
+        }
+    }
+    private static String askForGuess(){
+        printBar();
+        String guess = readString("Please enter your next guess.");
+        return guess; 
+    }
+    private static boolean ensureGuessCorrect(String guess, ArrayList<String> guessArrayList){
+        for(int i = 0; i < guess.length(); i++){
+            if(guess.equalsIgnoreCase("*****")){
+                return true;
+            }
+            else if(guess.length() > 5){
+                return false;
+            }
+            else if(doesStringContainOnlyChoices(guessArrayList)){
+                return false;
+            }
+            else if(areCharactersUnique(guessArrayList)){
+                return false;
+            }
+            else if(isGuessMissingAt(guessArrayList)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean doesStringContainOnlyChoices(ArrayList<String> guess){
+        for (String character : guess) {
+            if(!choicesSet.contains(guess)){
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean areCharactersUnique(ArrayList<String> guess){
+        boolean condition = false;
+        for(int i = 0; i < guess.size(); i++){
+            for(int j = 0; j < guess.size(); j++){
+                if(guess.get(i).equalsIgnoreCase(guess.get(j)) && i != j){
+                    condition = true;
+                }
+            }
+        }
+        return condition;
+    }
+    private static boolean isGuessMissingAt(ArrayList<String> guessArrayList){
+        if(!guessArrayList.contains("@")){
+            return true;
+        }
+        return false;
+    }
+    private static boolean doesPlayerGiveUp(String guess){
+        if(guess.equalsIgnoreCase("*****")){
+            return true;
+        }
+        return false;
+    }
+
+    private static void runPlayerCode(){
+        iGuess.createLists();        
+        println("You will make a code and the computer will guess it");
+        println("Please write down your 5 digit code with one @ symbol and four\n" +
+                "other unique characters fom the following list:\n" +choicesSet);
+        String score = "";
+        int guessCount = 0;
+        while(!score.equalsIgnoreCase("5A0B")){
+            ArrayList<String> currentGuess = iGuess.generateRandomGuess();
+            print("Guess #" + ++guessCount + "\t" + convertArrayListToString(currentGuess) +"\tEnter Score:");
+            score = readString("");
+            if(!score.equalsIgnoreCase("5A0B")){
+                iGuess.pruneList(currentGuess, score);
+            }            
+        }
+    }
+    private static void runComputerCodeAndGuess(){
+        iGuess.createLists();
+        ArrayList<String> computerSecretCode = iGuess.generateSecretCode();
+        println("The Computer will make a code and find a computer opponents code.");
+        String score = "";
+        int guessCount = 0;
+        while(!score.equalsIgnoreCase("5A0B")){
+            ArrayList<String> currentGuess = iGuess.generateRandomGuess();
+            score = iGuess.compareScores(currentGuess, computerSecretCode);
+            println("Guess #" + ++guessCount + "\t" + convertArrayListToString(currentGuess) +"\tEnter Score:" + score);
+            if(!score.equalsIgnoreCase("5A0B")){
+                iGuess.pruneList(currentGuess, score);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> stuff = iGuess.listOfCombinations;
+        for(int i = 0;i < 5; i++){
+            RunProgram();
+        }
+        // ArrayList<String> guess = convertStringToArrayList(askForGuess());
+        // ArrayList<String> secretCodeNew = new ArrayList<String>(Arrays.asList("v"))
+        // println(iGuess.compareScores(, combination))
     }
 }
