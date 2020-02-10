70c6505bbec3edc25acbb72d953d4fe15b7ab105
David <Sallazar024@gmail.com>
2020-02-10 11:08:59 -0500

    Day 7 Notes

---
+++ COP 3331/Day 7/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+
+//sequence containers by index
+#include <array>// c
+#include <vector> //c (best performing)
+#include <list> //nc
+#include <forward_list> //nc
+
+//adapters
+#include <queue> //fifo
+#include <stack> //lifo
+
+//Associative Containers by key
+#include <set> //NC
+#include <map>//nc
+//pair
+//multimap //nc
+//multiset //nc
+
+
+using namespace std;
+int main(){
+
+    vector<int> v{0,1,2,3,4,5};
+}
