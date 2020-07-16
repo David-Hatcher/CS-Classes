3bf234583703cee17efc87ce6031903de66000b0
David <Sallazar024@gmail.com>
2020-07-16 12:08:46 -0400

    Updating HW3

--- COP 4530/Homework 3/COP4530–HW#3.py
+++ COP 4530/Homework 3/COP4530–HW#3.py
@@ -24,32 +24,36 @@ class Node:
     #######################################
     #           Code I Added              #
     #######################################  
+    # printTree recursively prints out all of the tree noes
+    # with the correct indentation
     def printTree(self,num = 0):
-        print("    "*num,self.ID)
-        if(self.left != None):
-            self.left.printTree(num + 1)
-        if(self.right != None):
-            self.right.printTree(num + 1)
+        print("    "*num,self.ID) # 4 spaces * num argument and print node ID
+        if(self.left != None): # Check if left has a value
+            self.left.printTree(num + 1) # Calling itself on the left node, and incrementing num
+        if(self.right != None): # Check if right has a value
+            self.right.printTree(num + 1) # Calling itself on the right node, ad incrementing num
+    # mirror will recrusively swap all the left nodes with the right nodes and call
+    # itself on the next nodes in the tree
     def mirror(self):
-        global leftCounter,rightCounter,largestValue,smallestValue,nodeCount
-        self.left, self.right = self.right, self.left
-        nodeCount += 1
-        print("Parent: ",self.ID)
-        if(self.left != None):
-            leftCounter += 1
-            print("Left: ",self.left.ID)
-        if(self.right != None):
-            rightCounter += 1
-            print("Right: ",self.right.ID)
-        print()
-        if(self.ID > largestValue):
-            largestValue = self.ID
-        if(self.ID < smallestValue):
-            smallestValue = self.ID
-        if(self.right != None):
-            self.right.mirror()
-        if(self.left != None):
-            self.left.mirror()
+        global leftCounter,rightCounter,largestValue,smallestValue,nodeCount # setting global counters to increment
+        self.left, self.right = self.right, self.left # Swapping left and right pointers
+        nodeCount += 1 # Found a node, count it 
+        print("Parent: ",self.ID) # Printing the parent, which for the left and rights is this node
+        if(self.left != None): # Check if left has a pointer
+            leftCounter += 1 # Found a left node, count it
+            print("Left: ",self.left.ID) # Printing the left node name
+        if(self.right != None): # Check if right has a pointer
+            rightCounter += 1 # Found a right node, count it
+            print("Right: ",self.right.ID) # Printing the right node name
+        print() # putting a space afterwards
+        if(self.ID > largestValue): # Checking largestValue against the current node value
+            largestValue = self.ID # setting if greater
+        if(self.ID < smallestValue): # Checking smallest value against the current node value
+            smallestValue = self.ID # Setting if greater
+        if(self.right != None): # Checking if there is a right node
+            self.right.mirror() # Recursively calling itself on the right node
+        if(self.left != None): # Checking if there is a left node
+            self.left.mirror() # Recurisvely calling itself ont the left node
 
 
     #######################################
@@ -147,8 +151,10 @@ root.mirror()
 print("\n\n##################\nMirroring Tree\n##################\n\n")
 root.printTree()
 
+# Ending timer as program has completed requirements
 end = timer()
 
+# Printing out statistics of program
 print("**********************\n***** Statistics *****\n**********************")
 print("Number of Nodes: ",nodeCount)
 print("Number of Nodes On Left Side: ",leftCounter)
