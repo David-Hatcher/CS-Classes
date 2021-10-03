2dd649511d6576690658d3b77f765bcff69670e1
David Hatcher <sallazar024@gmail.com>
2021-10-03 12:59:47 -0400

    Adding GL-2

---
+++ CAP 4103 - Mobile Biometrics/GL2-HATCHER/get_images.py
@@ -0,0 +1,62 @@
+# Load imports
+import os 
+import cv2
+
+'''
+function get_images() definition
+Parameter, image_directory, is the directory 
+holding the images
+'''
+
+def increase_brightness(img, value=30):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    lim = 255 - value
+    v[v > lim] = 255
+    v[v <= lim] += value
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+def get_images(image_directory):
+    X = []
+    y = []
+    extensions = ('jpg','png','gif')
+    
+    '''
+    Each subject has their own folder with their
+    images. The following line lists the names
+    of the subfolders within image_directory.
+    '''
+    subfolders = os.listdir(image_directory)
+    for subfolder in subfolders:
+        print("Loading images in %s" % subfolder)
+        if os.path.isdir(os.path.join(image_directory, subfolder)): # only load directories
+            subfolder_files = os.listdir(
+                    os.path.join(image_directory, subfolder)
+                    )
+            for file in subfolder_files:
+                if file.endswith(extensions): # grab images only
+                    # read the image using openCV                    
+                    img = cv2.imread(
+                            os.path.join(image_directory, subfolder, file)
+                            )
+                    # resize the image                     
+                    width = 200
+                    height = 200
+                    dim = (width, height)
+                    img = cv2.resize(img, dim)
+                    # img = increase_brightness(img, value=50)
+                    # add the resized image to a list X
+                    X.append(img)
+                    # add the image's label to a list y
+                    y.append(subfolder)
+    
+    print("All images are loaded")     
+    # return the images and their labels      
+    return X, y
+                    
+                
+            
---
+++ CAP 4103 - Mobile Biometrics/GL2-HATCHER/main.py
@@ -0,0 +1,149 @@
+''' Imports '''
+def warn(*args, **kwargs):
+    pass
+import warnings
+
+warnings.warn = warn
+
+import numpy as np
+import copy as copy
+
+import get_images
+import get_landmarks
+
+''' Import classifier '''
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.naive_bayes import ComplementNB
+
+# NB, SVM, ANN
+
+''' Load the data and their labels '''
+img_dir_calTech = 'Caltech Faces Dataset'
+img_dir_sof = 'SoF Dataset'
+image_directory = img_dir_calTech
+X, y = get_images.get_images(image_directory)
+print(len(X))
+
+''' Get distances between face landmarks in the images '''
+# get_landmarks(images, labels, save_directory="", num_coords=5, to_save=False)
+X, y = get_landmarks.get_landmarks(X, y, 'landmarks/', 68, False)
+print(len(X))
+
+''' Matching and Decision '''
+# create an instance of the classifier
+# clf = SVC(kernel='poly',C=20,degree=4)
+# clf = KNeighborsClassifier(3)
+
+names = [
+    # "Nearest Neighbors", 
+    # "SVM",
+    # "RBF SVM",
+    # # "Gaussian Process",
+    # "Decision Tree",
+    # "Random Forest",
+    # "Neural Net",
+    "Gaussian Naive Bayes",
+    # "Catagorical Bayes",
+    # "Complement Bayes"
+    # "AdaBoost",
+    # "QDA",
+    ]
+'''  need to look closer at
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    "RBF SVM",
+'''
+classifiers = [
+    #n_neighbors=5,weights='distance',metric='euclidean',n_jobs=-1
+    # KNeighborsClassifier(3,n_jobs=-1),
+    # SVC(kernel="poly", C=1, degree=6)
+    # SVC(gamma=2, C=1),
+    # # GaussianProcessClassifier(1.0 * RBF(1.0)),
+    # DecisionTreeClassifier(max_depth=6),
+    # RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    # MLPClassifier(solver='lbfgs',max_iter=10000), #46%
+    GaussianNB(var_smoothing=1),
+    # CategoricalNB(),
+    # ComplementNB(),
+    # AdaBoostClassifier(),
+    # QuadraticDiscriminantAnalysis(),
+    ]
+
+
+
+for j in range(0,len(names)):
+    print()
+    print(names[j])
+    num_correct = 0
+    labels_correct = []
+    num_incorrect = 0
+    labels_incorrect = []
+    Xi = copy.deepcopy(X)
+    yi = copy.deepcopy(y)
+    clf = classifiers[j]
+    for i in range(0, len(yi)):
+        query_img = Xi[i, :]
+        query_label = yi[i]
+        
+        template_imgs = np.delete(Xi, i, 0)
+        template_labels = np.delete(yi, i)
+            
+        # Set the appropriate labels
+        # 1 is genuine, 0 is impostor
+        y_hat = np.zeros(len(template_labels))
+        y_hat[template_labels == query_label] = 1 
+        y_hat[template_labels != query_label] = 0
+        
+        clf.fit(template_imgs, y_hat) # Train the classifier
+        y_pred = clf.predict(query_img.reshape(1,-1)) # Predict the label of the query
+        
+        # Gather results
+        if y_pred == 1:
+            num_correct += 1
+            labels_correct.append(query_label)
+        else:
+            num_incorrect += 1
+            labels_incorrect.append(query_label)
+            # Print results
+
+    print("Num correct = %d, Num incorrect = %d, Accuracy = %0.2f" 
+        % (num_correct, num_incorrect, num_correct/(num_correct+num_incorrect)))    
+
+# labels_i_set = set(labels_incorrect)
+# labels_i_count = {}
+# for label in labels_i_set:
+#     labels_i_count[label] = labels_incorrect.count(label)
+
+# labels_c_set = set(labels_correct)
+# labels_c_count = {}
+# for label in labels_c_set:
+#     labels_c_count[label] = labels_correct.count(label)
+
+# labels_total_counts = []
+# labels_full_set = set.union(labels_c_set,labels_i_set)
+# for label in labels_full_set:
+#     correct_count, incorrect_count = 0, 0
+#     if(label in labels_c_count):
+#         correct_count = labels_c_count[label]
+#     if(label in labels_i_count):
+#         incorrect_count = labels_i_count[label]
+#     labels_total_counts.append((label,correct_count,incorrect_count,correct_count/(correct_count + incorrect_count)))
+
+# labels_total_counts.sort(key =  lambda tuple: tuple[3])
+
+# print(labels_total_counts)
+
+# Print results
+# print()
+# print("Num correct = %d, Num incorrect = %d, Accuracy = %0.2f" 
+#       % (num_correct, num_incorrect, num_correct/(num_correct+num_incorrect)))    
+    
+    
